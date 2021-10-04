ac596906963c966689059948a317b27813708a81
Maria Parra Segovia <majandra2404@gmail.com>
2021-10-04 08:27:15 -0300

    cambios en  archivos

--- src/Components/Main/Menu.jsx
+++ src/Components/Main/Menu.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { useState } from "react";
+import { Button,Card, CardTitle, CardText } from 'reactstrap';
+import axios from "axios";
+
 
 export default function Menu (){
+
+ 
+  const [users,setUsers]=useState([]);
+
+  const getUsers=()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+   .then(response=>{
+     console.log(response.data);
+     setUsers(response.data)
+   })
+   .catch(error=>{
+     console.log(error);
+   });
+
+  }
+
+ 
+
   return(
     <header>
-      <h3>
-        Y yo soy el menu!!!
-      </h3>
+      <Button color="success" onClick={getUsers}>boton</Button>
+     {
+       users.map((user)=>(
+        <Card key={user.id}>
+          <CardTitle>{user.name}</CardTitle>
+          <CardText>{user.email}</CardText>
+        </Card>
+       ))
+     }
     </header>
   )
 }
